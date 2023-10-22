041266e2b4b546c44c10da57beb4ed49ad6547cd
Moustapha DIOP <moustephdiop@gmail.com>
2023-10-22 20:33:33 +0200

    (refacto) notebook

--- helpers_allocation.py
+++ helpers_allocation.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from helpers_tickers import get_ticket_between_dates
 
 from pypfopt import EfficientFrontier
 from pypfopt import risk_models
@@ -7,7 +8,7 @@ from pypfopt import expected_returns
 from pypfopt.discrete_allocation import DiscreteAllocation, get_latest_prices
 from pypfopt import objective_functions
 
-from typing import Dict, Tuple
+from typing import Dict, Tuple, List
 
 
 def get_cumulative_returns(df: pd.DataFrame) -> pd.DataFrame:
@@ -104,3 +105,91 @@ def get_cumulative_returns_portfolio(df_tickers: pd.DataFrame, allocation: Dict)
     
     portfolio = df_assets.pct_change().mul(df_weights, axis=1).sum(axis=1)
     return (1 + portfolio).cumprod() - 1
+
+    
+def sell_all_stocks(df_tickers: pd.DataFrame, allocation: dict) -> float:
+    """
+    Calculates the total value of all stocks in the allocation dictionary
+    based on the latest price in the df_tickers DataFrame.
+
+    Args:
+        df_tickers (pd.DataFrame): A DataFrame containing the latest stock prices for each ticker.
+        allocation (dict): A dictionary containing the allocation of shares for each ticker.
+
+    Returns:
+        float: The total value of all stocks in the allocation dictionary.
+    """
+
+    total = 0
+    
+    for ticker, shares in allocation.items():
+        total += df_tickers[ticker].iloc[-1] * shares
+    
+    return total
+
+
+def portfolio_values(df_tickers: pd.DataFrame, allocation: dict) -> pd.DataFrame:
+    """
+    Calculates the total value of the portfolio based on the latest price in
+    the df_tickers DataFrame and the allocation of shares for each ticker.
+
+    Args:
+        df_tickers (pd.DataFrame): A DataFrame containing the latest stock prices for each ticker.
+        allocation (dict): A dictionary containing the allocation of shares for each ticker.
+
+    Returns:
+        pd.DataFrame: A DataFrame containing the total value of
+            the portfolio for each date in the df_tickers DataFrame.
+    """
+    df_assets = df_tickers[allocation.keys()]
+    df_weights = allocation.values()
+    portfolio = df_assets.mul(df_weights, axis=1).sum(axis=1)
+    
+    return portfolio
+
+
+def calculate_each_year_allocation(df_tickers: pd.DataFrame, nb_year_look_back: int) -> Tuple[List[float], pd.DataFrame]:
+    """
+    Calculates the allocation of a portfolio for each year based on the
+    latest stock prices in the df_tickers DataFrame.
+
+    Args:
+        df_tickers (pd.DataFrame): A DataFrame containing the latest stock prices for each ticker.
+        nb_year_look_back (int): The number of years to look back when calculating the allocation.
+
+    Returns:
+        Tuple[List[float], pd.DataFrame]: A tuple containing a list of the 
+        total value of the portfolio for each year and a DataFrame containing the 
+        total value of the portfolio for each date in the df_tickers DataFrame.
+    """
+
+    df_tickers_close = df_tickers["Close"]
+    
+    first_year = df_tickers_close.index[0].year
+    first_year_calculation = first_year + nb_year_look_back
+    last_year = df_tickers_close.index[-1].year
+    
+    money = 10000
+    dis_allocation = {}
+    
+    money_values = []
+    df_money = pd.DataFrame()
+    
+    for i in range(first_year_calculation, last_year + 1):
+        if dis_allocation != {}:
+            df_porforlio_value = portfolio_values(get_ticket_between_dates(df_tickers_close, 
+                                                                           str(i), str(i + 1)), 
+                                                  dis_allocation)
+            df_porforlio_value += money
+    
+            df_money = pd.concat([df_money, df_porforlio_value], axis=0)
+        
+        df_year = get_ticket_between_dates(df_tickers_close, str(i - nb_year_look_back), str(i + 1))
+        money += sell_all_stocks(df_year, dis_allocation)
+        money_values.append(money)
+        
+        allocation = optimize_portfolio(df_year, 0.1)
+        dis_allocation, money = discrete_allocation(df_year, allocation, money)        
+        
+    money += sell_all_stocks(df_tickers_close, dis_allocation) 
+    return money_values, df_money
