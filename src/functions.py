7c533df96492eb592400a3dfecef767b3b787945
Moustapha DIOP <moustephdiop@gmail.com>
2023-10-26 12:41:15 +0200

    (add) function to generate allocation

--- src/functions.py
+++ src/functions.py
@@ -1,4 +1,8 @@
 import pandas as pd
+from src.tickers import TickerManager
+from src.optimizer import optimize_portfolio
+from typing import Dict
+
 
 def get_cumulative_returns(df: pd.DataFrame) -> pd.DataFrame:
     """
@@ -12,3 +16,28 @@ def get_cumulative_returns(df: pd.DataFrame) -> pd.DataFrame:
     """
 
     return (1 + df.pct_change()).cumprod() - 1
+
+
+def generate_allocation(df_tickers: TickerManager, x_years_lb: int) -> Dict[int, Dict[str, float]]:
+    """Generates an allocation for each year between the first year and x_years_lb years after the first year.
+
+    Args:
+        df_tickers (TickerManager): A TickerManager object containing the asset returns.
+        x_years_lb (int): The number of years after the first year to generate allocations for.
+
+    Returns:
+        Dict[int, Dict[str, float]]: A dictionary containing the allocation for each year.
+    """
+
+    first_year = df_tickers.tickers.index[0].year
+    first_year_calculation = first_year + x_years_lb
+    last_year = df_tickers.tickers.index[-1].year
+    
+    allocation_years = {}
+    
+    for year in range(first_year_calculation, last_year):
+        df_x_year = df_tickers.tickers.loc[:str(year - 1)]
+        allocation = optimize_portfolio(df_x_year, 'max_sharpe', gamma=0.1)
+        allocation_years[year] = allocation
+        
+    return allocation_years
